f58489f3085bb997d00ae44d7b4b56fc4460782b
prathyu <melamprathyusha@gmail.com>
2024-04-19 17:54:27 +0530

    Added leap and resistorColor

---
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -0,0 +1,21 @@
+class ResistorColor {
+    int colorCode(String color) {
+        return switch (color) {
+            case "black" -> 0;
+            case "brown" -> 1;
+            case "red" -> 2;
+            case "orange" -> 3;
+            case "yellow" -> 4;
+            case "green" -> 5;
+            case "blue" -> 6;
+            case "violet" -> 7;
+            case "gray" -> 8;
+            case "white" -> 9;
+            default -> throw new IllegalStateException("Unexpected value: " + color);
+        };
+    }
+
+    String[] colors() {
+        return new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    }
+}
